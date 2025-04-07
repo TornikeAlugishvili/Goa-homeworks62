8ab29044fdf30c81aff80ee456197a9f278c9070
TornikeAlugishvili <alugishvili.24@gmail.com>
2025-04-07 12:25:44 +0400

    commit

---
+++ day 01/homework/homework1.py
@@ -0,0 +1,6 @@
+name="tornike"
+surname=" and his surname is maisuradze"
+height="is 152 cm"
+age="and his age is 11"
+knows_programming=True
+print(name +" "+str(height)+" " +str(surname) +" "+ str(age))
---
+++ day 03/classwork/classwork1.py
@@ -0,0 +1,5 @@
+name="Tornike"
+age=11
+height=152.7
+print(name+" "+str(age)+" "+str(height))
+print("Tornike is 11 years old and his height is 152.7")
