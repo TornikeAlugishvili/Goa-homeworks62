b6e05c2186536b7ba0af0588398845aa0a0fad00
TornikeAlugishvili <alugishvili.24@gmail.com>
2025-04-23 12:52:28 +0400

    commit

--- day 00/homework/homework.py
+++ day 00/homework/homework.py
@@ -1,92 +1,119 @@
 from turtle import *
 
-# we want to paint a house
+#we want to paint a house:
 
-#step 1:draw  a square
-speed(5)
-width (7)
+#step 1: positioning
+title("Drawing a House")
+
+shape("turtle")
+width(7)
+speed(3)
+bgcolor("Light blue")
+pencolor("green")
+penup()
+right(180)
+forward(190)
+right(180)
+pendown()
+forward(400)
+right(180)
+forward(100)
+#step 2: drawing a square
+
 begin_fill()
-color("brown")
-
-begin_fill()
-color("brown")
-
-forward (200)
+color("pink")
+pencolor("purple")
+right(90)
+forward(220)
 left(90)
-
-forward (200)
-left (90)
-
-
-forward (200)
+forward(220)
 left(90)
-
-forward (200)
-left (90)
-end_fill()   #end of square
-
-
-
-#drawing a door
-begin_fill()
+forward(220)
+end_fill()
+left(90)
+penup()
 forward(70)
-color("yellow")
 left(90)
-forward(120)   #height of the door
+pendown()
+
+#step 3: drawing a door
+begin_fill()
+color("brown")
+pencolor("black")
+forward(110)
 right(90)
 forward(60)
 right(90)
-forward(120)
+forward(110)
 end_fill()
 
-
+#step 4: positioning to floor
 penup()
-goto(200, 200)
-pendown()
-
-color("red")
-begin_fill()
-right(150)
-forward(200)
-left(120)
-forward(200)
-end_fill()
-
-begin_fill()    #drawing the window 1
-color("blue")
-
-begin_fill()
+left(90)
+forward(89)
+left(90)
+forward(220)
 left(30)
-pendown()
-forward(80)
-left(90)
-forward(60)
-left(90)
-forward(80)  #end of the window 1
-end_fill()
 
-right(90)      #drawing the window 2
-forward(75)
+#step5: drawing a floor
+
 begin_fill()
 color("blue")
+pencolor("black")
+pendown()
+forward(180)
+left(110)
+forward(198)
+end_fill()
+
+#step6: positioning to window 1
+
+left(35)
+penup()
+forward(50)
+left(90)
+begin_fill()
+color("Light blue")
+pencolor("blue")
+forward(20)
+width(4)
+pendown()
+forward(30)
 right(90)
-forward(80)
-left(90)
-forward(60)   
-left(90)
-forward(80)
-left(90)
-forward(60)
-end_fill()      #end of window 2
+
+forward(30)
+right(90)
+
+forward(30)
+right(90)
+
+forward(30)
+right(90)
+end_fill()
+
+
+#step7: positioning to window 2
 
 penup()
-left(90)
-forward(29)
+forward(190)
+right(180)
 
 
+begin_fill()
+color("Light blue")
+pencolor("blue")
+forward(30)
+right(90)
+pendown()
 
+forward(30)
+right(90)
 
+forward(30)
+right(90)
 
-
-
+forward(30)
+right(90)
+forward(10)
+end_fill()
 exitonclick()
