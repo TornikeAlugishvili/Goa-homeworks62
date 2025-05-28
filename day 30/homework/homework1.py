bd699bf4dd067c9a99248b2c011aea283a40338e
TornikeAlugishvili <alugishvili.24@gmail.com>
2025-05-28 20:16:03 +0400

    commit

---
+++ day 30/homework/homework1.py
@@ -0,0 +1,6 @@
+list = ["Sport", "Food", "Telephone", "Name", "House"]
+Index = int(input('Enter number: '))
+if Index >= len(list):
+    print("gurami error")
+else:
+    print(list[Index])
---
+++ day 32/classwork/classwork1.py
@@ -0,0 +1,9 @@
+String1 = "Tornike"
+String2 = "Demetre"
+String3 = "Andria"
+String4 = "saba"
+
+print(String1.lower())
+print(String2.upper())
+print(String3.capitalize())
+print(String4.replace("a", "b"))
