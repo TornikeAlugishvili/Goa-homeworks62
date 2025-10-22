be13224d6923a99b08152acc109b28bc96dcf3c0
TornikeAlugishvili <alugishvili.24@gmail.com>
2025-10-22 18:57:49 +0400

    main

--- day 65.5/classwork/my_module.py
+++ day 65.5/classwork/my_module.py
@@ -1,3 +1,3 @@
 def func(num):
     return list(filter(lambda i: i % 2 == 1, num))
-name = 'deimuri'
+name = 'Tornike'
--- day 68/homework/.py
+++
@@ -1,9 +0,0 @@
-from turtle import *
-
-count = 0
-while True:
-    speed(25)
-    count += 0.5
-    forward(count)
-    left(220)
-    print(count)
